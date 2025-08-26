2e0b5872072b6cdd55cd781e2c36328383ba3a9f
szidanne <sawsanzidanne@gmail.com>
2025-08-26 13:13:30 +0300

    #minor - i hope u die tailwind

--- task_manager_django/settings.py
+++ task_manager_django/settings.py
@@ -202,6 +202,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR / "staticfiles")
+STATICFILES_DIRS = [
+    BASE_DIR / "theme" / "static_src",  # This is where django-tailwind outputs compiled CSS
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
